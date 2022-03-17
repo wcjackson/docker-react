18fe6ff860c548a3fba0ddf426e4223508bc6fed
Corey Jackson <coreytjackson@gmail.com>
2022-03-17 17:51:40 -0400

    Another one.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Good bye!
+          Another other one.
         </p>
         <a
           className="App-link"
