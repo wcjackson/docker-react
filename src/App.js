0675083bf5066754a236ca0e747638734bff300b
wcjackson <coreytjackson@gmail.com>
2022-06-12 14:28:34 +0000

    Gitpod test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Another other one.
+          Test GitPod.
         </p>
         <a
           className="App-link"
